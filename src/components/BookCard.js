fdd5215ca25a6eaec40ccb464d01c0bb9e9a32d7
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-12-04 11:57:23 -0700

    style images

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -3,8 +3,8 @@ import "./style.css";
 import image from '../images/books.jpg';
 import { Card, Icon, Image } from 'semantic-ui-react';
 
-const BookCard = () => (
-  <Card>
+const BookCard = (props) => (
+  <Card onclick={props.clicked}>
     <Image src={image} />
     <Card.Content>
       <Card.Header>Books</Card.Header>
