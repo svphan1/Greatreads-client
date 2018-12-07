00625d6c15995230513c37ddba517af9d546001e
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-12-06 17:07:55 -0700

    adding post methods to book form

--- src/components/Books/BookAddForm.js
+++ src/components/Books/BookAddForm.js
@@ -3,17 +3,61 @@ import { Form } from "semantic-ui-react";
 import BookHeader from "./BookHeader";
 
 class BookAddForm extends Component {
+  state = {
+    title: "",
+    authors: "",
+    description: "",
+    coverUrl: ""
+  };
   // handleChange = (e, { value }) => this.setState({ value })
+  titleListener = event => {
+    this.setState({ title: event.target.value });
+    console.log(this.state.title);
+  };
+
+  authorsListener = event => {
+    this.setState({ authors: event.target.value });
+    console.log(this.state.authors);
+  };
+
+  descriptionListener = event => {
+    this.setState({ description: event.target.value });
+    console.log(this.state.description);
+  };
+
+  coverUrlListener = event => {
+    this.setState({ coverUrl: event.target.value });
+    console.log(this.state.coverUrl);
+  };
 
   render() {
     return (
       <Form>
         <Form.Group widths="equal">
-          <Form.Input fluid label="Book Name" placeholder="Book name" />
-          <Form.Input fluid label="Authors" placeholder="Author name" />
+          <Form.Input
+            fluid
+            label="Book Name"
+            placeholder="Book name"
+            onChange={this.titleListener}
+          />
+          <Form.Input
+            fluid
+            label="Authors"
+            placeholder="Author name"
+            onChange={this.authorsListener}
+          />
         </Form.Group>
-        <Form.TextArea label="Description" placeholder="Book description..." />
-        <Form.Input fluid label="Image Url" placeholder="Image Url" />
+        <Form.TextArea
+          label="Description"
+          placeholder="Book description..."
+          onChange={this.descriptionListener}
+        />
+        <Form.Input
+          fluid
+          label="Image Url"
+          placeholder="Image Url"
+          onChange={this.coverUrlListener}
+        />
         <Form.Button>Add Book</Form.Button>
       </Form>
     );
